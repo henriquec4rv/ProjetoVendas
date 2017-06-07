924c47bec7b36848b3fbe4bc130476df8eef36ef
Luis Felipe Oliveira de Araújo <felipelfoliveira@gmail.com>
2017-06-07 17:32:42 +0000

    Remocao de imports não utilizados

---
+++ VendaMain.java
@@ -0,0 +1,80 @@
+import java.io.FileNotFoundException;
+
+public class VendaMain {
+	
+	public static void main(String[] args) {
+
+		MenuVendas menu = new MenuVendas();
+		Vendedor vendedor = new Vendedor();
+		Produto produto = new Produto();
+		Venda venda = new Venda();
+		ArqMan arq = new ArqMan();
+
+
+		while (menu.getOpt() != 4) {
+
+			menu.setOpt2(2);
+			menu.vassourinha();
+			menu.menu1();
+			switch (menu.getOpt()) {
+
+			case 1: // Registrando uma Venda
+
+				while (menu.getOpt2() == 2) {
+
+					venda.registrarVenda(venda);
+					venda.mostrarVenda(venda);
+					try {
+						arq.salvar(venda, "vendas/" + venda.getVendedor().getNome());
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+					menu.menu2();
+
+				}
+				break;
+
+			case 2: // Registrando um novo Vendedor
+
+				while (menu.getOpt2() == 2) {
+
+					menu.vassourinha();
+					vendedor.cadastroVendedor(vendedor);
+					try {
+						arq.salvar(vendedor, "vendedores/" + vendedor.getCodigo());
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					vendedor.exibeVendedor(vendedor);
+					menu.menu2();
+				}
+				break;
+
+			case 3: // Registrando um novo Produto
+
+				while (menu.getOpt2() == 2) {
+					produto.criarProduto(produto);
+					try {
+						arq.salvar(produto, "produtos/" + produto.getCodProduto());
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					produto.mostraProduto(produto);
+					menu.menu2();
+
+				}
+				break;
+
+			case 4: // Saindo do sistema
+				System.exit(0);
+
+			default:
+				System.out.println("Informe um valor dentre as opcoes: ");
+				break;
+			}
+		}
+	}
+
+}
